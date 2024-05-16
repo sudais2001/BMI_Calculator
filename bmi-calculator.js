14a034b0cb177056a6f07211bcafd17fa11fc251
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-17 02:19:05 +0500

    Add files via upload

---
+++ bmi-calculator.js
@@ -0,0 +1,5 @@
+let weight = 45;
+let height = 5.3;
+let bmi = weight / (height * height);
+console.log(`Sudais Your Bmi is ${bmi}`);
+export {};
---
+++ bmi-calculator.ts
@@ -0,0 +1,6 @@
+let weight = 45;
+let height = 5.3;
+
+let bmi = weight/ (height*height)
+
+console.log(`Sudais Your Bmi is ${bmi}`)
